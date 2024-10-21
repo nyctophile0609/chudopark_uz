06a52cca2468dbd1e8521fd8ac11d03f27ea9599
space_cowboy <uchihajavohir0609@gmail.com>
2024-10-21 19:54:40 +0500

    chudopark/V1/Default api/No errors yet/DB(MYSQL==> SQLite)/

--- chudopark/entrypoint.sh
+++ chudopark/entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-while ! mysqladmin ping -h"mysql" --silent; do
-    echo "Waiting for MySQL..."
-    sleep 2
-done
+# while ! mysqladmin ping -h"mysql" --silent; do
+#     echo "Waiting for MySQL..."
+#     sleep 2
+# done
 
 echo "Apply database migrations"
 python3 manage.py makemigrations
 python3 manage.py migrate
-echo $DJANGO_SUPERUSER_USERNAME
-echo $DJANGO_SUPERUSER_EMAIL
-echo $DJANGO_SUPERUSER_PASSWORD
 
 echo "Creating superuser"
 python3 manage.py shell << END
